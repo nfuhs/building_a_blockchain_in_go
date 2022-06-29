c1833006ec0004ec12004472792af9b8edcc1933
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2022-06-29 17:40:45 +0200

    Part 3 Added BadgerDB

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nfuhs/building_a_blockchain_in_go
 
 go 1.15
+
+require github.com/dgraph-io/badger v1.6.2
--- main.go
+++ main.go
@@ -1,57 +1,103 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"flag"
 	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+
+	"github.com/nfuhs/building_a_blockchain_in_go/blockchain"
 )
 
-type BlockChain struct {
-	blocks []*Block
+type CommandLine struct {
+	blockchain *blockchain.BlockChain
 }
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
+//printUsage will display what options are availble to the user
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage: ")
+	fmt.Println(" add -block <BLOCK_DATA> - add a block to the chain")
+	fmt.Println(" print - prints the blocks in the chain")
 }
 
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+//validateArgs ensures the cli was given valid input
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		//go exit will exit the application by shutting down the goroutine
+		// if you were to use os.exit you might corrupt the data
+		runtime.Goexit()
+	}
 }
 
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
+//addBlock allows users to add blocks to the chain via the cli
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("Added Block!")
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
+//printChain will display the entire contents of the blockchain
+func (cli *CommandLine) printChain() {
+	iterator := cli.blockchain.Iterator()
+
+	for {
+		block := iterator.Next()
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("hash: %x\n", block.Hash)
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+		// This works because the Genesis block has no PrevHash to point to.
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
 }
 
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
+//run will start up the command line
+func (cli *CommandLine) run() {
+	cli.validateArgs()
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+	// Parsed() will return true if the object it was used on has been called
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock(*addBlockData)
+	}
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
 }
 
 func main() {
-	chain := InitBlockChain()
+	defer os.Exit(0)
 
-	chain.AddBlock("First Block after Genesis")
-	chain.AddBlock("Second Block after Genesis")
-	chain.AddBlock("Third Block after Genesis")
+	chain := blockchain.InitBlockChain()
+	defer chain.Database.Close()
 
-	for _, block := range chain.blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-	}
+	cli := CommandLine{chain}
+
+	cli.run()
+
 }
